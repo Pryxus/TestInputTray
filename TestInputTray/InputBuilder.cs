2e28652aacc27901d656c867cc35dc724c76aedc
Pryxus <166079411+Pryxus@users.noreply.github.com>
2025-02-13 14:08:27 +0100

    First upload of the project

---
+++ TestInputTray/InputBuilder.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace TestInputTray
+{
+    internal class InputBuilder : IEnumerable<SimMouse>
+    {
+        private readonly List<SimMouse> _inputList = new List<SimMouse>();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint SendInput(uint numberOfInputs, SimMouse[] inputs, int sizeOfInputStructure);
+
+        public SimMouse[] ListToArray()
+        {
+            return _inputList.ToArray();
+        }
+
+        public IEnumerator<SimMouse> GetEnumerator()
+        {
+            return _inputList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public InputBuilder AddToMousePos(int x, int y)
+        {
+            var movement = new SimMouse
+            {
+                Type = 0,
+                Data = new MouseHardware
+                {
+                    Mouse = new Mouseinput
+                    {
+                        X = x,
+                        Y = y,
+                        Scan = 0,
+                        Flags = 0x0001,
+                        Time = 0,
+                        DwExtraInfo = IntPtr.Zero
+                    }
+                }
+            };
+
+            _inputList.Add(movement);
+
+            return this;
+        }
+
+        public void MoveMouseBy(int pixelDeltaX, int pixelDeltaY)
+        {
+            AddToMousePos(pixelDeltaX, pixelDeltaY);
+            var mouseArray = ListToArray();
+
+            if (mouseArray.Length == 0)
+            {
+                throw new InvalidOperationException("Empty array received");
+            }
+
+            var success = SendInput((uint)mouseArray.Length, mouseArray, Marshal.SizeOf<SimMouse>());
+            if (success != mouseArray.Length)
+            {
+                int errorCode = Marshal.GetLastWin32Error();
+                throw new InvalidOperationException($"Library Access Error. Error Code: {errorCode}");
+            }
+        }
+    }
+
+    internal struct SimMouse
+    {
+        public uint Type;
+        public MouseHardware Data;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct MouseHardware
+    {
+        [FieldOffset(0)]
+        public Mouseinput Mouse;
+    }
+
+    internal struct Mouseinput
+    {
+        public int X;
+        public int Y;
+        public uint Scan;
+        public uint Flags;
+        public uint Time;
+        public IntPtr DwExtraInfo;
+    }
+}
---
+++ TestInputTray/Program.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Runtime.InteropServices;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace TestInputTray
+{
+    internal class Program
+    {
+        private static NotifyIcon _notifyIcon;
+        private static ContextMenuStrip _cms;
+        private static ApplicationContext _context;
+
+        private const int TimerIntervall = 30000;
+        private static readonly System.Timers.Timer Timer = new System.Timers.Timer();
+        private static bool _runs = true;
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern uint SetThreadExecutionState(ExecutionState esFlags);
+
+        [STAThread]
+        private static void Main()
+        {
+            SystemEvents.SessionSwitch += HandleSessionSwitch;
+
+            _notifyIcon = new NotifyIcon();
+            _notifyIcon.Icon = Properties.Resources.runs;
+            _notifyIcon.Text = @"TestInput";
+
+            _cms = new ContextMenuStrip();
+
+            _cms.Items.Add(new ToolStripMenuItem("Status ändern", null, ChangeState_Click));
+            _cms.Items.Add(new ToolStripSeparator());
+            _cms.Items.Add(new ToolStripMenuItem("Beenden", null, Quit_Click, "Quit"));
+
+            SetThreadExecutionState(ExecutionState.EsContinuous | ExecutionState.EsDisplayRequired | ExecutionState.EsAwaymodeRequired);
+
+            Timer.Interval = TimerIntervall;
+            Timer.Elapsed += OnTimerElapsed;
+            Timer.AutoReset = false;
+            Timer.Start();
+
+            _notifyIcon.ContextMenuStrip = _cms;
+            _notifyIcon.Visible = true;
+            _notifyIcon.DoubleClick += ChangeState_Click;
+
+            _context = new ApplicationContext();
+            Application.Run(_context);
+
+            _notifyIcon.Visible = false;
+        }
+
+        private static void HandleSessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            switch (e.Reason)
+            {
+                case SessionSwitchReason.SessionLock:
+                case SessionSwitchReason.ConsoleDisconnect:
+                    Timer.Stop();
+                    break;
+                case SessionSwitchReason.SessionUnlock:
+                case SessionSwitchReason.ConsoleConnect:
+                    Timer.Start();
+                    break;
+            }
+        }
+
+        private static void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            var s = new InputBuilder();
+            s.MoveMouseBy(1, 1);
+            s.MoveMouseBy(-1, -1);
+
+            Timer.Interval = TimerIntervall;
+            Timer.Start();
+        }
+
+        private static void ChangeState_Click(object sender, EventArgs e)
+        {
+            if (_runs)
+            {
+                SetThreadExecutionState(ExecutionState.EsContinuous);
+                _notifyIcon.Icon = Properties.Resources.stops;
+                Timer.Stop();
+            }
+            else
+            {
+                SetThreadExecutionState(ExecutionState.EsContinuous | ExecutionState.EsDisplayRequired);
+                _notifyIcon.Icon = Properties.Resources.runs;
+                Timer.Start();
+            }
+            _runs = !_runs;
+        }
+
+        private static void Quit_Click(object sender, EventArgs e)
+        {
+            SetThreadExecutionState(ExecutionState.EsContinuous);
+            _context.ExitThread();
+        }
+
+    }
+
+    [Flags]
+    public enum ExecutionState : uint
+    {
+        EsAwaymodeRequired = 64,
+        EsContinuous = 2147483648,
+        EsDisplayRequired = 2,
+    }
+}
